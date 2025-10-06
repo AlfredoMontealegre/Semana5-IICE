891d2a969afff0d0a9a38e60e2d508949494deac
Blanca <bazeledon@uamv.edu.ni>
2025-10-06 07:15:03 -0600

    Archivos BLANCA

---
+++ Viernes/Ejercicio 3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intercalar_arreglos
+{
+    internal class Program
+    {
+
+        //Intercalar arreglos 
+        //Enunciado: Solicita dos arreglos de n elementos cada uno.Crea un tercer arreglo que combine ambos intercalando los valores.
+        //Identificar origen de cada valor 
+        //Después de crear el arreglo intercalado, muestra para cada número si proviene de arr1 o arr2.
+
+        static void Main(string[] args)
+        {
+            // n es el tammaño de los arreglos
+            int n = 10;
+            int[] array1 = new int[n];
+            int[] array2 = new int[n];
+            int[] array3 = new int[n * 2];
+
+            Console.WriteLine("Ingrese los 10 números del primer arreglo:");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                
+                Console.Write($"Número {i + 1}: ");
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Ingrese los 10 números del segundo arreglo:");
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write($"Número {i + 1}: ");
+                array2[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Intercambiando los arreglos...");
+
+            int j = 0; //Se crea otra variable para el arreglo 3
+            for (int i = 0; i < 10; i++)
+            {
+                array3[j] = array1[i];
+                Console.WriteLine(array1[i] + " (arr1)");
+                j++; //Se incrementa j para que no se sobreescriba el valor anterior
+                array3[j] = array2[i];
+                Console.WriteLine(array2[i] + " (arr2)");
+                j++;
+
+            }
+            Console.ReadKey(); //Pausa para ver el resultado
+
+        }
+    }
+}
---
+++ Viernes/Ejercicio1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Ejercicio1
+{
+    internal class Temperatura
+    {
+        static void Main()
+        {
+            string[] dias = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+            double[] temperaturaC = new double[7];
+            double[] temperaturaF = new double[7];
+
+            // Solicitar temperaturas en °C
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write($"Escribe la temperatura para {dias[i]} (°C): ");
+
+                // TryParse significa que intenta convertir la entrada del usuario a un número double. Y si no puede, devuelve false en lugar de lanzar una excepción.
+                // El out significa que la variable temperaturaC[i] se pasará por referencia. Luego, se asignará el valor convertido si la conversión es correcta.
+                while (!double.TryParse(Console.ReadLine(), out temperaturaC[i]))
+                {
+                    Console.Write("Valor inválido. Intenta de nuevo: ");
+                }
+                temperaturaF[i] = (temperaturaC[i] * 9 / 5) + 32;
+            }
+
+            // Mostrar ambos arreglos en paralelo
+            // Nombre de las columnas
+            Console.WriteLine("\nDía\t\t°C\t\t°F");
+            for (int i = 0; i < 7; i++)
+            {
+                // \t es para que se imprima en columnas
+                Console.WriteLine($"{dias[i],-10}\t{temperaturaC[i],6:F1}\t{temperaturaF[i],8:F1}");
+            }
+
+            // Estadísticas
+            // Average es un método que calcula el promedio de una secuencia de valores numéricos
+            double promedio = temperaturaC.Average();
+
+            // Días con temperatura > 30°C y < 0°C, t es igual a la abreviación de temperatura
+            int diasSobre30 = temperaturaC.Count(t => t > 30); 
+            int diasBajoCero = temperaturaC.Count(t => t < 0);
+
+            // IndexOf es un método que busca el índice de un elemento específico en una matriz o lista, en este caso de la tem min y max
+            int diaMasFrio = Array.IndexOf(temperaturaC, temperaturaC.Min());
+            int diaMasCaluroso = Array.IndexOf(temperaturaC, temperaturaC.Max());
+
+            Console.WriteLine($"\nTemperatura promedio: {promedio:F2} °C");
+            Console.WriteLine($"Día más frío: {dias[diaMasFrio]} ({temperaturaC[diaMasFrio]:F1} °C)");
+            Console.WriteLine($"Día más caluroso: {dias[diaMasCaluroso]} ({temperaturaC[diaMasCaluroso]:F1} °C)");
+            Console.WriteLine($"Días por encima de 30°C: {diasSobre30}");
+            Console.WriteLine($"Días bajo cero: {diasBajoCero}");
+        }
+    }
+}
