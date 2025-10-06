cfcf2558020a5afca03956223cb21b916ad4388f
AlfredoMontealegre <montealfredo24@gmail.com>
2025-10-06 07:16:37 -0600

    Ejercicio 3 [REPARADO]

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,16 +1,32 @@
 ﻿// Ejercicio 3
 // Intercalar dos arrays
 
-using System.Collections.Immutable;
+using System;
 
-Console.WriteLine("-----> Intercalacion de Arrays <-----");
-int[] arr1 = [1, 3, 5, 7, 9];
-int[] arr2 = [2, 4, 6, 8, 10];
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("-----> Intercalación de Arrays <-----");
 
-int[] arr3 =
-    [.. arr1, .. arr2];
+        int[] arr1 = { 1, 3, 5, 7, 9 };
+        int[] arr2 = { 2, 4, 6, 8, 10 };
 
-Console.WriteLine("Array 1: " + string.Join(", ", arr1));
-Console.WriteLine("Array 2: " + string.Join(", ", arr2));
-Console.WriteLine("Array Intercalado: " + string.Join(", ", Array.Sort(arr3));
-Console.ReadKey();
+        int[] arr3 = new int[arr1.Length + arr2.Length];
+
+        int index = 0;
+
+        // Intercalar los elementos
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            arr3[index++] = arr1[i];
+            arr3[index++] = arr2[i];
+        }
+
+        Console.WriteLine("Array 1: " + string.Join(", ", arr1));
+        Console.WriteLine("Array 2: " + string.Join(", ", arr2));
+        Console.WriteLine("Array Intercalado: " + string.Join(", ", arr3));
+
+        Console.ReadKey();
+    }
+}
